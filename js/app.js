fa9095de984a4136b884d2e4e7b2030084707a9e
Cesar Gomez <00.13.zkr@gmail.com>
2015-11-22 17:49:02 -0600

    QR function

--- js/app.js
+++ js/app.js
@@ -88,17 +88,28 @@ function principal(){}
 function nuevaMesa(){
     $("#qrCode").on("click",QRCode);
     function QRCode(){
-        cordova.plugins.barcodeScanner.scan(
-          function (result) {
-              alert("We got a barcode\n" +
-                    "Result: " + result.text + "\n" +
-                    "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);
-          }, 
-          function (error) {
-              alert("Scanning failed: " + error);
-          }
-       );
+        alert('scanning');        
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        scanner.scan( function (result) { 
+            alert("We got a barcode\n" + 
+            "Result: " + result.text + "\n" + 
+            "Format: " + result.format + "\n" + 
+            "Cancelled: " + result.cancelled);  
+
+           console.log("Scanner result: \n" +
+                "text: " + result.text + "\n" +
+                "format: " + result.format + "\n" +
+                "cancelled: " + result.cancelled + "\n");
+            document.getElementById("info").innerHTML = result.text;
+            console.log(result);
+            /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+        }, function (error) { 
+            console.log("Scanning failed: ", error); 
+        } );
     }
 }
 $.fn.serializeObject = function()
